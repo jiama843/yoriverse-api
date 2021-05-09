b4c619e8868e1986cb5b7b8770023f1c0d995322
Jia <jiama843@gmail.com>
2021-05-09 15:45:23 -0400

    migration to add more fields to character

---
+++ db/migrate/20210509175055_add_features_to_character_table.rb
@@ -0,0 +1,14 @@
+class AddFeaturesToCharacterTable < ActiveRecord::Migration[5.1]
+  def change
+    add_column :characters, :first_name, :string
+    add_column :characters, :last_name, :string
+    add_column :characters, :age, :integer
+    add_column :characters, :height, :decimal
+    add_column :characters, :weight, :decimal
+    add_column :characters, :date_of_birth, :datetime
+    # add_column :characters, :location_of_birth, :string # This should probably be an association to locations table
+    add_column :characters, :description, :string
+
+    remove_column :characters, :relationships # going to become preloaded association
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210429022941) do
+ActiveRecord::Schema.define(version: 20210509175055) do
 
   create_table "characters", force: :cascade do |t|
     t.string "appearance"
     t.string "traits"
-    t.string "relationships"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.decimal "height"
+    t.decimal "weight"
+    t.datetime "date_of_birth"
+    t.string "description"
   end
 
 end
