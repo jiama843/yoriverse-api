d40bf7a3bba7406055a5ab8676fac75034080c71
Jia <jiama843@gmail.com>
2021-05-10 14:06:31 -0400

    removed unapplicable tests

--- test/controllers/character_controller_test.rb
+++ test/controllers/character_controller_test.rb
@@ -3,18 +3,18 @@
 require 'test_helper'
 
 class CharacterControllerTest < ActionDispatch::IntegrationTest
-  test 'should get appearance' do
-    get character_appearance_url
-    assert_response :success
-  end
+  # test 'should get appearance' do
+  #   get character_appearance_url
+  #   assert_response :success
+  # end
 
-  test 'should get traits' do
-    get character_traits_url
-    assert_response :success
-  end
+  # test 'should get traits' do
+  #   get character_traits_url
+  #   assert_response :success
+  # end
 
-  test 'should get relationships' do
-    get character_relationships_url
-    assert_response :success
-  end
+  # test 'should get relationships' do
+  #   get character_relationships_url
+  #   assert_response :success
+  # end
 end
