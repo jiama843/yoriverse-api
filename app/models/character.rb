d36d07e2c86a63e45790cbd3baa2d18fae9b0c39
Jia <jiama843@gmail.com>
2021-05-20 13:19:22 -0400

    readded samples

--- app/models/character.rb
+++ app/models/character.rb
@@ -9,4 +9,23 @@ class Character < ApplicationRecord
   has_many :relation_froms, class_name: 'Relationship', foreign_key: :character_to_id, inverse_of: :character_to,
                             dependent: :destroy
   has_many :character_froms, through: :relation_froms, source: :character_from
+
+  # scope :with_relationships, -> {  }
+
+  def to_formatted_json do
+    byebug
+    {
+        id: id,
+        name: {
+            first: first_name,
+            last: last_name,
+            full: `{first_name} {last_name}`
+        },
+        
+    }
+  end
+
+  private
+
+  def 
 end
